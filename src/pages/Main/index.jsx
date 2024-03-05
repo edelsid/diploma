756c9da57b0efe685e9bac3d2e67de67d4232c6d
Xenia Ch <eradonna@yandex.ru>
2024-03-05 20:16:07 +0300

    temporary menu

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,6 +1,7 @@
 import About from "./About"
 import Feedback from "./Feedback"
 import Rules from "./Rules"
+import { Link } from "react-router-dom"
 import "./main.css"
 
 export default function Main() {
@@ -9,6 +10,13 @@ export default function Main() {
       <About />
       <Rules />
       <Feedback />
+      <div className="temporary">
+        <Link className="link" to={'/seats'}>seats</Link>
+        <Link className="link" to={'/passengers'}>passengers</Link>
+        <Link className="link" to={'/payment'}>payment</Link>
+        <Link className="link" to={'/accept'}>accept</Link>
+        <Link className="link" to={'/confirm'}>confirm</Link>
+      </div>
     </>
   )
 }
